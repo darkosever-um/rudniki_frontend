52049c9c88349bfb22fd541038b825f3f0d25d22
darkosever-um <darko.sever@student.um.si>
2025-04-29 10:33:33 +0200

    IR-55 - zemljevid modul dodan Maps.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Maps from './modules/Maps'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Maps></Maps>
+    </div>
+  );
+}
+
+export default App;
