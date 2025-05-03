2f29ea09be3c2e65424120aa848b13dc038b68e0
darkosever-um <darko.sever@student.um.si>
2025-05-03 11:24:52 +0200

    IR-63 Stranski meni dodan, in povezana funkcionalnost z navigacijskim menijem

--- src/modules/Nav.js
+++ src/modules/Nav.js
@@ -27,8 +27,50 @@ function Nav() {
           <button className="text-2xl" aria-label="Profil"><AccountCircleIcon/></button>
         </div>
       </div>
-
       
+      {/* Stranski meni */}
+      {menuOpen && (
+        <>
+          <div className="fixed top-0 left-0 h-full w-64 bg-white shadow-md z-20 flex flex-col justify-between p-4">
+            <div>
+              <span className="text-xs opacity-30 font-bold mb-4">ORODJA</span>
+              <ul className="space-y-2">
+                <li><a href="/zemljevid" className="block hover:underline">Zemljevid</a></li>
+                <li><a href="/rudniki" className="block hover:underline">Rudniki</a></li>
+                <li><a href="/statistika" className="block hover:underline">Statistika rudnikov</a></li>
+              </ul>
+
+              <br/>
+
+              <span className="text-xs opacity-30 font-bold mb-4">NASTAVITVE</span>
+              <ul className="space-y-2">
+                <li><a href="/profil" className="block hover:underline">Profil</a></li>
+                <li><a href="/nastavitve-videza" className="block hover:underline">Izgled aplikacije</a></li>
+              </ul>
+
+              <br/>
+
+              <span className="text-xs opacity-30 font-bold mb-4">POMOČ</span>
+              <ul className="space-y-2">
+                <li><a href="/faq" className="block hover:underline">FAQ</a></li>
+                <li><a href="/kontakt" className="block hover:underline">Kontakt</a></li>
+              </ul>
+            </div>
+
+            <div>
+              <OurButton
+                onClickDo={() => alert('Odjava')} // TODO @darkosever : dodaj funkcionalnost
+                // className="w-full mt-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
+                text={"Odjava"}
+                variant='red'
+                classNameProps='w-full mt-4'
+              >
+                Odjava
+              </OurButton>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
